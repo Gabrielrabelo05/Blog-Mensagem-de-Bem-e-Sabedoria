d95577306e9fe0541dcc43392dde8f9f2379987a
darknessstonight <dudisfonseca.16@gmail.com>
2024-01-30 14:17:42 -0300

    update js

--- SRC/login/login.js
+++ SRC/login/login.js
@@ -5,7 +5,7 @@
 
 //     const url = "http://localhost:8080/perfil";
 //     const data = { username, password };
-//    
+   
 //     const response = await fetch(url, {
 //         method: 'GET',
 //         headers: {
@@ -22,26 +22,67 @@
 //         alert('Usuário ou senha incorretos');
 //     }
 // }
-async function fazerLogin() {
-    const username = document.getElementById('usuario-log').value;
-    const password = document.getElementById('senha-log').value;
-    console.log(username)
+// async function fazerLogin() {
+//     const username = document.getElementById('usuario-log').value;
+//     const password = document.getElementById('senha-log').value;
+//     console.log(username)
     
-    const url = `http://localhost:8080/perfil?username=${username}&password=${password}`;
-    // const url = "http://localhost:8080/perfil";
+    // const url = `http://localhost:8080/perfil?username=${username}&password=${password}`;
+//     // const url = "http://localhost:8080/perfil";
 
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+//     const response = await fetch(url, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
 
-    const result = await response.json();
+//     const result = await response.json();
     
-    if (result.status === 'ok') {
-        console.log('Login bem-sucedido');
-    } else {
-        console.log('Usuário ou senha incorretos');
+//     if (result.status === 'ok') {
+//         console.log('Login bem-sucedido');
+//     } else {
+//         console.log('Usuário ou senha incorretos');
+//     }
+// }
+
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+
+function validarForm() {
+    if (username.value == "") {
+        alert("endereço de email invalido")
+        return false
     }
+    if (password.value, 1, 9) {
+        alert("A senha precisa ter entre 1 e 8 caracteres")
+        return false
+    }
+    return true
+}
+async function auth(){
+    if(validarForm()){
+        const user = {
+            "dataSearchName" : username.value,
+            "dataSearchPassword" : password.value
+        }
+            const respo = await authUser(user)
+            .then(promise => {
+                if(promise.status == 404) alert(promise.message);
+                if(promise.status == 401) alert(promise.message)
+
+                console.log(promise);          
+            }) 
+    }
+}
+async function authUser(obj){
+    const header =
+    { 
+        headers: { "Content-Type": "application/json", 
+        Accept: "application/json" },
+        method: "POST",
+        body : JSON.stringify(obj)
+    }
+        const enviar = await fetch('http://localhost:8080/perfil/login',header).then(promise => promise.json())
+        return enviar
 }
